12892ae7b840efb0f31f3d0faa68fcde21027c72
Israel Gadelha <gdelha.israel@gmail.com>
2021-11-01 17:44:55 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/israelgda" target="_blank" rel="noreferrer">Israel Gadelha</a></p>
+                <p className="text-light">Perfil do linkedin <a href="https://www.linkedin.com/in/israel-gadelha/" target="_blank" rel="noreferrer">Acesse meu linkedin</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
